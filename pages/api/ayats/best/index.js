083adac2aa449bd571197866acf9abc1bdef8767
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2020-02-17 00:45:30 +0500

    #upd best ayat name

--- pages/api/ayats/best/index.js
+++ pages/api/ayats/best/index.js
@@ -3,11 +3,11 @@ import { query } from 'lib/database';
 
 import sample from 'lodash/sample';
 
-const profilesApi = async (request, response) => {
+const getBestAyat = async (request, response) => {
   const allAyats = await query(escape`SELECT * FROM mainpage_ayats`);
   const bestAyat = sample(allAyats);
 
   response.status(200).json({ bestAyat });
 };
 
-export default profilesApi;
+export default getBestAyat;
