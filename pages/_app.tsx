f5f81176097c293b643b6c7abfc37e92c7547a3e
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2020-03-06 22:38:59 +0500

    #upd config for production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import c from 'classnames';
 
 import 'styles/lib/reset.styl';
@@ -11,6 +12,13 @@ import Header from 'components/header';
 function QuranOnlineApp({ Component, pageProps }: AppProps) {
   return (
     <div className={c(css.siteContainer, 'darkTheme')}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700|PT+Sans:400,400i,700&display=swap&subset=cyrillic"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
